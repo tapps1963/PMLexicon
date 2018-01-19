a730d17a8a63b479999d4d4b612ac4204a8fb2f8
shayne <shayne@icon.co.za>
2018-01-19 16:43:55 +0200

    Commit

--- JavaGit02/src/package2/MyClass.java
+++ JavaGit02/src/package2/MyClass.java
@@ -2,37 +2,57 @@ package package2;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
+
+import javax.swing.JOptionPane;
 
 public class MyClass {
 
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	public static void main(String[] args) {
+
+		String jarCheck = MyClass.class.getResource("MyClass.class").toString().substring(0, 4);
+
+		System.out.println(jarCheck);
+		String appConfigPath;
 		
-		String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-		String appConfigPath  = rootPath + "app.properties";
-		
+		if (jarCheck != "jar:") {
+			appConfigPath = "resources/" + "app.properties";
+		} else {
+
+			appConfigPath = "C:\\JavaUtilities\\" + "app.properties";
+		}
+
 		Properties appProps = new Properties();
-		
+
 		try {
 			appProps.load(new FileInputStream(appConfigPath));
-			
+
 			String appUser = appProps.getProperty("user");
 			String appVersion = appProps.getProperty("name", "Default Shayne");
 			String appGroup = appProps.getProperty("group", "Default Group");
-			String appDownloadAddr = appProps.getProperty("downloadAddr","Doef doef");
-			
+			String appDownloadAddr = appProps.getProperty("downloadAddr", "Doef doef");
+
+			JOptionPane.showMessageDialog(null, jarCheck, "Test", 0);
+
 			System.out.println(appUser);
 			System.out.println(appVersion);
 			System.out.println(appGroup);
 			System.out.println(appDownloadAddr);
 			
-			
+			System.out.println(System.getProperty("os.name"));
+
+			Properties p = System.getProperties();
+			p.list(System.out);
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			JOptionPane.showMessageDialog(null, jarCheck, "Test", 0);
 			e.printStackTrace();
 		}
-		
-		
 
 	}
 
