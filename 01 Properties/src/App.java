5ce17e857ef46710822504d98d0a09c5769d0028
shayne <shayne@icon.co.za>
2018-01-22 15:51:43 +0200

    Commit

--- 01 Properties/src/App.java
+++ 01 Properties/src/App.java
@@ -1,24 +1,35 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 import javax.swing.JOptionPane;
 
 public class App {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 
-		Properties prop = new Properties();
-		//InputStream is = new FileInputStream("/Users/shayne/eclipse-workspace-01/01 Properties/my.properties");
-		InputStream is = new FileInputStream("my.properties");
-		prop.load(is);
+		App sample = new App();
 		
-		//System.out.println(prop.getProperty("uname"));
-		
-		//JOptionPane.showMessageDialog(null, prop.getProperty("uname"));
-		prop.list(System.out);;
+		sample.loadProps1();
+
 	}
 
+	private void loadProps1() {
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream("app.properties");
+		
+		try {
+			Properties prop = new Properties();
+			prop.load(is);
+			
+			JOptionPane.showMessageDialog(null, "Made it");
+			prop.list(System.out);
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+			System.out.println(e);
+		}
+	}
+		
 }
+
