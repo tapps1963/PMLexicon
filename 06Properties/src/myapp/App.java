12e9921b15ea9ec10d70c51bd5d8193162a1ff62
shayne <shayne@icon.co.za>
2018-01-23 06:14:35 +0200

    Commit

--- 06Properties/src/myapp/App.java
+++ 06Properties/src/myapp/App.java
@@ -1,8 +1,6 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+package myapp;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Properties;
 
 import javax.swing.JOptionPane;
@@ -18,42 +16,11 @@ public class App {
 
 	private static void saveMyProp() {
 		// Properties file path.
-		String filePath = "app.properties";
-		Properties prop = new Properties();
-
-		try (InputStream outputStream = App.class.getClassLoader().getResourceAsStream(filePath)) {
-
-			// Loading the properties.
-			prop.load(outputStream);
-
-			// Getting properties
-			String user = prop.getProperty("user");
-			String password = prop.getProperty("password");
-			String dbase = prop.getProperty("dbase");
-			String serverinternal = prop.getProperty("serverinternal");
-			String serverexternal = prop.getProperty("serverexternal");
-
-			// Output to console the properties
-			System.out.println("User Name = " + user);
-			System.out.println("Password = " + password);
-			System.out.println("Database = " + dbase);
-			System.out.println("Internal IP = " + serverinternal);
-			System.out.println("External URL = " + serverexternal);
-
-			JOptionPane.showMessageDialog(null, "Read Ok");
-
-		} catch (IOException ex) {
-			System.out.println("Problem occurs when reading file !");
-			JOptionPane.showMessageDialog(null, "Did not make it");
-			ex.printStackTrace();
-		}
-
-
 	}
 
 	private static void readMyProp() {
 		// Properties file path.
-		String filePath = "app.properties";
+		String filePath = "myapp/app.properties";
 		Properties prop = new Properties();
 
 		try (InputStream inputStream = App.class.getClassLoader().getResourceAsStream(filePath)) {
@@ -75,7 +42,7 @@ public class App {
 			System.out.println("Internal IP = " + serverinternal);
 			System.out.println("External URL = " + serverexternal);
 
-			JOptionPane.showMessageDialog(null, "Read Ok");
+			JOptionPane.showMessageDialog(null, user);
 
 		} catch (IOException ex) {
 			System.out.println("Problem occurs when reading file !");
